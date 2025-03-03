2e21caff444735c9699f7aed764102d16dd4ce83
Adam Nelson <anelsonfs@outlook.com>
2025-03-03 14:47:36 +0000

    Temp fetching implemented

--- resources/js/temperature.js
+++ resources/js/temperature.js
@@ -1,12 +1,27 @@
 function getTemperature() {
     console.log("Fetching temperature data");
     try{
+        placeName = document.getElementById("city").value;
         fetch("resources/data/sample.json")
         .then((response) => response.json())
         .then((data) => {
-            document.getElementById("temp").innerHTML = data.temperature;
+            placeTemp = searchByName(data, placeName)
+            document.getElementById("temp").innerText = placeTemp
         });
     } catch (error) {
         console.error(error);
     }
+}
+
+function searchByName(dataIn, name)
+{
+    for(i = 0; i < dataIn.length; i++)
+    {
+        if(dataIn[i].cityName == name)
+        {
+            return dataIn[i].temperatureCelsius;
+        }
+    }
+
+    return "Not found";
 }
